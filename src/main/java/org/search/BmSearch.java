0ad36b3875de60b8dec888b4f7178d5fce887471
Frank Giordano <giofr01@yahoo.com>
2023-05-02 11:28:21 -0400

    first main checkin

---
+++ src/main/java/org/search/BmSearch.java
@@ -0,0 +1,62 @@
+package org.search;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * This performs a Boyer Moore search of a pattern within a string of text data
+ * and returning the position location of the pattern within the string if any.
+ * <p>
+ * Boyer-Moore algorithm, which looks first for the final letter of the target string,
+ * and uses a lookup table to tell it how far ahead it can skip in the input whenever
+ * it finds a non-matching character.
+ *
+ * <p>
+ * 0(n/m) sublinear runtime but worst case can be O(n*m)
+ *
+ * @author Frank Giordano
+ */
+public class BmSearch {
+
+    private final String pattern;
+    private final Map<Character, Integer> misMatchShiftsTable = new HashMap<>();
+
+    public BmSearch(String pattern) {
+        this.pattern = pattern;
+        this.compileMisMatchShiftsTable();
+    }
+
+    private void compileMisMatchShiftsTable() {
+        final int lengthOfPattern = pattern.length();
+        for (int i = 0; i < lengthOfPattern; i++) {
+            misMatchShiftsTable.put(pattern.charAt(i), Math.max(1, lengthOfPattern - i - 1));
+        }
+    }
+
+    public int findPosition(String text) {
+        final int lengthOfPattern = pattern.length();
+        final int lengthOfText = text.length();
+        int numOfSkips;
+
+        for (int i = 0; i <= lengthOfText - lengthOfPattern; i += numOfSkips) {
+            numOfSkips = 0;
+            for (int j = lengthOfPattern - 1; j >= 0; j--) {  // check starting from right to left
+                if (pattern.charAt(j) != text.charAt(i + j)) {
+                    if (misMatchShiftsTable.get(text.charAt(i + j)) != null) {
+                        numOfSkips = misMatchShiftsTable.get(text.charAt(i + j));
+                    } else {
+                        numOfSkips = misMatchShiftsTable.size();
+                    }
+                    break;
+                }
+            }
+
+            if (numOfSkips == 0) {  // means we found the matching position
+                return i;
+            }
+        }
+
+        return lengthOfText; // meaning have not found a match
+    }
+
+}
---
+++ src/main/java/org/search/Grep.java
@@ -0,0 +1,116 @@
+package org.search;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Implementation of a basic grep linux like command.
+ * <p>
+ * This program follows GNU grep logic flow. It reads raw data into a large buffer,
+ * searches the buffer using Boyer-Moore, and only when it finds a match
+ * does it go and look for the bounding newlines.
+ *
+ * @author Frank Giordano
+ */
+public class Grep {
+
+    private static String fileLocation;
+    private static String pattern;
+    private static BmSearch search;
+
+    private static String getFileContent(File file) throws IOException {
+        final StringBuilder content = new StringBuilder();
+        try (final BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                content.append(line).append("\n");
+            }
+        }
+        return content.toString();
+    }
+
+    private static List<String> search(String content) {
+        final List<String> results = new ArrayList<>();
+        int index = search.findPosition(content);
+        while (index != 0) {
+            final String foundStr = content.substring(index);
+
+            final StringBuilder entireLine = new StringBuilder();
+            for (int i = index - 1; i >= 0; i--) {
+                if (content.charAt(i) == '\n') {
+                    break;
+                }
+                entireLine.append(content.charAt(i));
+            }
+            entireLine.reverse();
+            for (int i = 0; i < foundStr.length(); i++) {
+                if (foundStr.charAt(i) == '\n') {
+                    break;
+                }
+                entireLine.append(foundStr.charAt(i));
+            }
+
+            if (entireLine.length() > 0) {
+                results.add(entireLine.toString());
+            }
+            int newIndex = index + pattern.length();
+            if (newIndex > content.length()) {
+                break;
+            }
+            content = content.substring(index + pattern.length());
+            index = search.findPosition(content);
+        }
+        return results;
+    }
+
+    private static void setup(String[] args) {
+        try {
+            fileLocation = args[0];
+        } catch (Exception e) {
+            throw new IllegalArgumentException("missing first file or directory argument, try again...");
+        }
+        try {
+            pattern = args[1];
+        } catch (Exception e) {
+            throw new IllegalArgumentException("missing second search pattern argument, try again...");
+        }
+        search = new BmSearch(pattern);
+    }
+
+    public static void main(String[] args) throws IOException {
+        try {
+            setup(args);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return;
+        }
+
+        final File file = new File(fileLocation);
+        List<String> files = new ArrayList<>();
+        if (file.isDirectory()) {
+            files = Stream.of(file.listFiles())
+                    .filter(item -> !item.isDirectory())
+                    .map(File::getName)
+                    .collect(Collectors.toList());
+        }
+
+        if (files.isEmpty()) {
+            search(getFileContent(file)).forEach(System.out::println);
+            return;
+        }
+
+        files.sort(Comparator.naturalOrder());
+        for (String name : files) {
+            search(getFileContent(new File(name)))
+                    .forEach(item -> System.out.println(name + ":" + item));
+        }
+    }
+
+}
