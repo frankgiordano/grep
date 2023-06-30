a7a73cbc166807b4a947c8d35ae074cf5b4b7d5a
Frank Giordano <giofr01@yahoo.com>
2023-06-29 23:51:05 -0400

    doc tweak

--- src/main/java/com/search/Grep.java
+++ src/main/java/com/search/Grep.java
@@ -14,7 +14,7 @@ import java.util.stream.Stream;
  * Implementation of a basic grep linux like command.
  * <p>
  * This program follows GNU grep logic flow. It reads raw data into a large buffer,
- * searches the buffer using Boyer-Moore, and only when it finds a match
+ * searches the buffer using Boyer-Moore algorithm, and only when it finds a match
  * does it go and look for the bounding newlines.
  *
  * @author Frank Giordano
